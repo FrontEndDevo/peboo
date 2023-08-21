a3d1bcec976ee496f2df36adcaf40bb47cc2cc1e
FrontEndDevo <mylinkacco@gmail.com>
2023-08-21 15:43:35 +0300

    Add google fonts (Work-Sans & Roboto)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <>
-      <h1 className="text-red-400 text-9xl">Vite + React</h1>
+      <h1 className="text-red-400 text-9xl font-work-sans">Vite + React</h1>
     </>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      Roboto: "Roboto",
+      "work-sans": "Work Sans",
+    },
   },
   plugins: [],
 };
