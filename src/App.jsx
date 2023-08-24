94e2e59dc07d7d76e0920e63f566d9a3919dfd49
FrontEndDevo <mylinkacco@gmail.com>
2023-08-24 17:34:08 +0300

    Create Header component & add header images

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Header from "./components/Header";
+
 const App = () => {
   return (
     <>
-      <h1 className="text-red-400 text-9xl font-work-sans">Vite + React</h1>
+      <Header />
     </>
   );
 };
