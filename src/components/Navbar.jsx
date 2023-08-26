6b19359d9ebdc40c2999762d1c8ef395a04b1e01
FrontEndDevo <mylinkacco@gmail.com>
2023-08-26 17:44:31 +0300

    Finish the styles for Navbar component without responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,44 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
 const Navbar = () => {
+  const [navbarBackground, setNavbarBackground] = useState(false);
+
   const navbarLinks = ["solutions", "pricing", "resources", "sign-up"].map(
     (link, i) => (
-      <li key={i}>
+      <li
+        key={i}
+        className={`duration-200 ${
+          navbarBackground ? "hover:text-indigo-200" : "hover:text-indigo-700"
+        }`}
+      >
         <a href={link}>
           {link.charAt(0).toUpperCase() + link.slice(1).replace("-", " ")}
         </a>
       </li>
     )
   );
+
+  useEffect(() => {
+    const changeNavbarBackground = () => {
+      setNavbarBackground(window.scrollY >= 100 ? true : false);
+    };
+
+    window.addEventListener("scroll", changeNavbarBackground);
+  }, []);
+
   return (
-    <nav>
-      <div className="logo">
-        <h1>Peboo</h1>
+    <nav
+      className={`fixed top-0 left-0 right-0 z-50 duration-200 rounded-b-[3rem] ${
+        navbarBackground && "bg-indigo-900 text-white"
+      }`}
+    >
+      <div className="container mx-auto flex items-center justify-between py-10">
+        <div className="logo">
+          <h1 className="font-work-sans text-3xl ">Peboo</h1>
+        </div>
+        <ul className="links flex gap-20">{navbarLinks}</ul>
       </div>
-      <ul className="links">{navbarLinks}</ul>
     </nav>
   );
 };
