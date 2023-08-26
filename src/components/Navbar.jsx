fb9030035b8145d257a9e1775f4d98e2c497b884
FrontEndDevo <mylinkacco@gmail.com>
2023-08-26 17:17:27 +0300

    Set the JSX for Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,21 @@
 const Navbar = () => {
-  return <nav>Navbar</nav>;
+  const navbarLinks = ["solutions", "pricing", "resources", "sign-up"].map(
+    (link, i) => (
+      <li key={i}>
+        <a href={link}>
+          {link.charAt(0).toUpperCase() + link.slice(1).replace("-", " ")}
+        </a>
+      </li>
+    )
+  );
+  return (
+    <nav>
+      <div className="logo">
+        <h1>Peboo</h1>
+      </div>
+      <ul className="links">{navbarLinks}</ul>
+    </nav>
+  );
 };
 
 export default Navbar;
