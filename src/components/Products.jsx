e2ac9acedbffe8488ce526769956bd33ff106e33
FrontEndDevo <mylinkacco@gmail.com>
2023-08-26 12:38:48 +0300

    Edit JSX to render all products using map method

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -6,44 +6,43 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+const ourProducts = [
+  {
+    icon: faShapes,
+    title: "Roadmap to success",
+    description:
+      "Design your future. Share your strategy with the people responsible for delivering it. Create clarity and buy-in.",
+  },
+  {
+    icon: faBullseye,
+    title: "Goals that matter",
+    description:
+      "Design your future. Share your strategy with the people responsible for delivering it. Create clarity and buy-in.",
+  },
+  {
+    icon: faTrophy,
+    title: "Visible achievements",
+    description:
+      "Design your future. Share your strategy with the people responsible for delivering it. Create clarity and buy-in.",
+  },
+];
+
 const Products = () => {
+  const renderedProducts = ourProducts.map((product, index) => (
+    <div key={index}>
+      <FontAwesomeIcon icon={product.icon} />
+      <h4>{product.title}</h4>
+      <p>{product.description}</p>
+      <button>
+        Learn More <FontAwesomeIcon icon={faArrowRightLong} />
+      </button>
+    </div>
+  ));
+
   return (
     <section>
       <h2>One product, three solutions</h2>
-      <div>
-        <div>
-          <FontAwesomeIcon icon={faShapes} />
-          <p>
-            Design your future. Share your strategy with the people responsible
-            for delivering it. Create clarity and buy-in.
-          </p>
-          <button>
-            Learn More <FontAwesomeIcon icon={faArrowRightLong} />
-          </button>
-        </div>
-        <div>
-          <FontAwesomeIcon icon={faBullseye} />
-          <h4>Goals that matter</h4>
-          <p>
-            Design your future. Share your strategy with the people responsible
-            for delivering it. Create clarity and buy-in.
-          </p>
-          <button>
-            Learn More <FontAwesomeIcon icon={faArrowRightLong} />
-          </button>
-        </div>
-        <div>
-          <FontAwesomeIcon icon={faTrophy} />
-          <h4>Visible achievements</h4>
-          <p>
-            Design your future. Share your strategy with the people responsible
-            for delivering it. Create clarity and buy-in.
-          </p>
-          <button>
-            Learn More <FontAwesomeIcon icon={faArrowRightLong} />
-          </button>
-        </div>
-      </div>
+      <div>{renderedProducts}</div>
     </section>
   );
 };
