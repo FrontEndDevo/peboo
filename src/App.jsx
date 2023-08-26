385096c2847de9a958fa9f3aded8fa5d9cf90794
FrontEndDevo <mylinkacco@gmail.com>
2023-08-26 17:10:05 +0300

    Create Navbar component and add it to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Features from "./components/Features";
 import Header from "./components/Header";
+import Navbar from "./components/Navbar";
 import Products from "./components/Products";
 import Statistics from "./components/Statistics";
 
 const App = () => {
   return (
     <>
+      <Navbar />
       <Header />
       <Products />
       <Features />
