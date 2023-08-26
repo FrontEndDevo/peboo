fcab4f7a6bd3df28c1cfdcd7c5208e85ceb183d2
FrontEndDevo <mylinkacco@gmail.com>
2023-08-26 13:24:15 +0300

    Finish JSX of Features component with get the (read more) button ready from the previous component

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,5 +1,24 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import allFeatures from "../assets/images/features.png";
+import { faArrowRightLong } from "@fortawesome/free-solid-svg-icons";
 const Features = () => {
-  return <section>Features</section>;
+  return (
+    <section>
+      <div>
+        <img src={allFeatures} alt="all features" />
+      </div>
+      <div>
+        <h2>Finally a place where it all comes together</h2>
+        <p>
+          Bring together your strategy with the goals that need to deliver your
+          strategy, and the people that need to deliver those goals.
+        </p>
+        <button className="text-indigo-600 flex items-center justify-center gap-2 hover:underline mx-auto xl:mx-auto">
+          Learn More <FontAwesomeIcon icon={faArrowRightLong} />
+        </button>
+      </div>
+    </section>
+  );
 };
 
 export default Features;
