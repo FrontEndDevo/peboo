b3dabb7a7c31bf7bc2cdb9ac6256fa9bb7464cbd
FrontEndDevo <mylinkacco@gmail.com>
2023-08-27 16:25:40 +0300

    Finish responsive design for Navbar component but still logic of opening and closing menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,9 +8,11 @@ const Navbar = () => {
     (link, i) => (
       <li
         key={i}
-        className={`duration-200 ${
-          navbarBackground ? "hover:text-indigo-200" : "hover:text-indigo-700"
-        }`}
+        className={`duration-100 p-2 rounded-full border-dotted border-4 border-indigo-700 md:border-none ${
+          navbarBackground
+            ? "hover:text-indigo-200 hover:border-0"
+            : "hover:text-indigo-700"
+        } ${i == 3 && "text-indigo-700 w-20 flex justify-center items-center"}`}
       >
         <a href={link}>
           {link.charAt(0).toUpperCase() + link.slice(1).replace("-", " ")}
@@ -29,15 +31,23 @@ const Navbar = () => {
 
   return (
     <nav
-      className={`fixed top-0 left-0 right-0 z-50 duration-200 rounded-b-[3rem] ${
-        navbarBackground && "bg-indigo-900 text-white"
+      className={`fixed top-0 left-0 right-0 z-50 duration-200 rounded-b-[3rem] mx-4 mt-4 md:m-0 ${
+        navbarBackground && "md:bg-indigo-900 md:text-white"
       }`}
     >
-      <div className="container mx-auto flex items-center justify-between py-10">
-        <div className="logo">
-          <h1 className="font-work-sans text-3xl ">Peboo</h1>
+      <div className="container w-fit relative md:mx-auto md:w-full flex items-center justify-between md:py-10">
+        <div className="relative">
+          <h1 className="font-work-sans rounded p-1 pr-8 text-white text-3xl bg-gradient-to-r from-indigo-700">
+            Peboo
+          </h1>
+          <span className="absolute top-1/3 right-0 flex h-3 w-3">
+            <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+            <span className="relative inline-flex rounded-full h-3 w-3 bg-indigo-400"></span>
+          </span>
         </div>
-        <ul className="links flex gap-20">{navbarLinks}</ul>
+        <ul className="origin-bottom-right flex gap-12 md:gap-20 flex-row-reverse items-center md:flex-row absolute top-0 right-1/2 -rotate-90 md:rotate-0 md:relative md:right-0">
+          {navbarLinks}
+        </ul>
       </div>
     </nav>
   );
