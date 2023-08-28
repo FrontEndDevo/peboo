02e72bdb360c4b552cad129c97ba02cadcf40a43
FrontEndDevo <mylinkacco@gmail.com>
2023-08-28 16:03:34 +0300

    Responsive Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -22,8 +22,10 @@ const footerHyperLinks = [
 const socialMedia = [faFacebookF, faInstagram, faTwitter, faLinkedinIn];
 const Footer = () => {
   const hyperLinks = footerHyperLinks.map((obj, index) => (
-    <div key={index}>
-      <h3 className="font-work-sans text-3xl mb-4">{obj.topic}</h3>
+    <div key={index} className="text-center lg:text-start">
+      <h3 className="font-work-sans text-3xl mb-4 border-indigo-600 border-b-4 p-2 rounded-2xl lg:border-none">
+        {obj.topic}
+      </h3>
       <ul>
         {obj.links.map((link) => (
           <li key={link} className="my-5">
@@ -54,24 +56,28 @@ const Footer = () => {
       <img
         src={bottomBackground}
         alt="footer bottom background"
-        className="w-32 h-20 absolute bottom-12 right-12 -rotate-90"
+        className="w-32 h-20 absolute bottom-10 right-2 lg:bottom-12 lg:right-12 -rotate-90"
       />
       <img
         src={topBackground}
         alt="footer top background"
         className="absolute -top-5 left-0"
       />
-      <div className="container mx-auto py-24 grid grid-cols-3 justify-items-end">
-        <div className="">
+      <div className="container mx-auto py-24 grid grid-cols-1 xl:grid-cols-3 xl:justify-items-end">
+        <div className="text-center xl:text-start">
           <h2 className="font-work-sans text-3xl">Peboo</h2>
-          <p className="my-8 text-base xl:text-xl w-2/3">
+          <p className="mx-auto xl:mx-0 my-8 text-lg xl:text-xl w-2/3">
             Our simple but powerful OKR+ platform turns great strategies into
             amazing results. And it&apos;s free.
           </p>
-          <div className="flex gap-6 text-xl">{pebooSocialMedia}</div>
+          <div className="mb-12 xl:mb-0 flex justify-center xl:justify-start gap-6 text-xl">
+            {pebooSocialMedia}
+          </div>
         </div>
         <div className="col-span-2">
-          <div className="flex gap-40">{hyperLinks}</div>
+          <div className="flex gap-12 md:gap-40 flex-col md:flex-row items-center md:items-start justify-center xl:justify-start">
+            {hyperLinks}
+          </div>
         </div>
       </div>
     </footer>
